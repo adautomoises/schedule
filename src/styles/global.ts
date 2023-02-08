95c4207bb128f0fe9659ea03a16a5e7772281201
adautomoises <adautomaleandro@gmail.com>
2023-02-07 21:39:41 -0300

    Scroll Bar estilizada

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,4 +14,23 @@ export const GlobalStyle = createGlobalStyle`
   main {
     color: ${({ theme }) => theme.colors.black100};
   }
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: rgba(133, 133, 133, 0.18);
+    border-radius: 12px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: rgba(136, 136, 136, 0.32);
+    border-radius: 12px;
+
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: rgba(133, 133, 133, 0.49);
+  }
 `;
