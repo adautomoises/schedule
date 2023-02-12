6f4e0183639094686eb89ab6f0cca6348e2a0115
adautomoises <adautomaleandro@gmail.com>
2023-02-12 19:21:11 -0300

    Rotas Privadas e Públicas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,8 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import { useAuth } from "./context/userContext";
+
+import { DefaultLayout } from "./layouts/Default/DefaultLayout";
+import { ProfileLayout } from "./layouts/Profile/ProfileLayout";
 
 import { Home } from "./pages/Home";
 import { SignIn } from "./pages/SignIn";
@@ -6,20 +10,32 @@ import { SignUp } from "./pages/SignUp";
 import { Contacts } from "./pages/Contacts";
 import { Reminders } from "./pages/Reminders";
 import { Friends } from "./pages/Friends";
-
-import { DefaultLayout } from "./layouts/DefaultLayout";
+import { Profile } from "./pages/Profile";
+import { Events } from "./pages/Events";
+import { Calendar } from "./pages/Calendar";
 
 const Router = () => {
+  const { user } = useAuth();
+  const userUUID = localStorage.getItem("@schedule:user-uuid-1.0.0");
+
   return (
     <BrowserRouter>
+
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/entrar" element={<SignIn />} />
           <Route path="/cadastrar" element={<SignUp />} />
+        </Route>
+        <Route path="/" element={user || userUUID ? <DefaultLayout /> : <Navigate to="/entrar" />}>
           <Route path="/contatos" element={<Contacts />} />
           <Route path="/notas" element={<Reminders />} />
           <Route path="/amigos" element={<Friends />} />
+          <Route path="/eventos" element={<Events />} />
+          <Route path="/calendario" element={<Calendar />} />
+        </Route>
+        <Route path="/perfil" element={user || userUUID ? <ProfileLayout /> : <Navigate to="/entrar" />}>
+          <Route path="*" element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
